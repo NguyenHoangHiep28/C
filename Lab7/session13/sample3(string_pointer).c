c98b1b124ecb57dab1504b5700cfc17838b23974
NguyenHoangHiep28 <71130020+NguyenHoangHiep28@users.noreply.github.com>
2020-10-01 19:16:07 +0700

    Lab7

---
+++ Lab7/session13/sample3(string_pointer).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main()
+{
+	char a, str[81], *ptr;
+	printf("\nEnter a sentence:");
+	gets(str);
+	printf("\nEnter character to search for:");
+	a = getchar();
+	ptr = strchr(str, a);
+	
+	//return pointer to char
+	printf("\nString starts at address : %u", str);
+	printf("\nFirst occurrence of the character is at address: %u", ptr);
+	printf("\nPosition of first occurrece (starting from 0) is: %d", ptr - str);
+	
+}
